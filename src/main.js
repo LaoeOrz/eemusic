1aa1c4149233ea457818ddba6aec61e665297a0b
laoeOrz <1059014839@qq.com>
2020-06-15 06:31:05 +0800

    第一章节基础样式完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+// import router from './router'
 
-Vue.config.productionTip = false
+import "common/stylus/index.styl";
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
+  el: "#app",
   render: h => h(App)
-})
+});
